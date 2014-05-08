840893e5f5ba9fc70b07027829609febc3e40a97
Tiger Oakes <tigeroakes@gmail.com>
2014-05-07 19:37:14 -1000

    Adding support for phonegap build. Minor whitespace changing in index.html and route.js.  Removed unused holiday class from css

--- js/route.js
+++ js/route.js
@@ -4,13 +4,13 @@
 //	Calling getQueryVariable("image") - would return "awesome.jpg".
 function getQueryVariable(variable)
 {
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] === variable){return pair[1];}
-       }
-       return(false);
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] === variable){return pair[1];}
+	}
+	return(false);
 }
 
 var jsonUrl = "../data.json";
