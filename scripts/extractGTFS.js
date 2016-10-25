3aaff39f74808a202bfe76827c3a3ec275f1be4d
Tiger Oakes <tigeroakes@gmail.com>
2016-10-24 19:50:04 -0700

    Updated extracter code to convert some files to objects instead of arrays, and removed unneeded newlines in output

--- scripts/extractGTFS.js
+++ scripts/extractGTFS.js
@@ -1,35 +1,75 @@
-const { createReadStream, createWriteStream, stat } = require('fs');
-const { join, extname } = require('path');
+const { createReadStream, createWriteStream, stat: statCb } = require('fs');
+const { join, extname, basename } = require('path');
+const { Transform } = require('stream');
+const { StringDecoder } = require('string_decoder');
 const { Parse } = require('unzip');
 const { Converter } = require('csvtojson');
+const mkdirp = require('mkdirp');
 
-function promisePipe(source, dest) {
-	return new Promise((resolve, reject) => {
-		const writeStream = createWriteStream(dest);
-		writeStream.on('finish', resolve).on('error', reject);
-
-		const conveter = new Converter({
-			constructResult: false,
-			toArrayString: true,
-			ignoreEmpty: true,
-			flatKeys: true,
-		});
-
-		source.pipe(conveter).pipe(writeStream);
-	});
+function streamToPromise(stream, endEvent = 'finish') {
+  return new Promise((resolve, reject) => {
+    stream.on(endEvent, resolve);
+  	stream.on('error', reject);
+  });
 }
 
-function exists(path) {
-	return new Promise((resolve, reject) => {
-		stat(path, (err, result) => {
-			if (err) {
-				if (err.code === 'ENOENT') resolve(false);
-				else reject(err);
-			}
+class ArrayToObject extends Transform {
+  constructor(basename, options) {
+    super(options);
+		this.decoder = new StringDecoder('utf8');
 
-			resolve(true);
-		})
-	})
+		switch (basename) {
+			case 'agency': case 'stops': case 'routes':	case 'trips':
+				this.keypath =
+					`${basename.endsWith('s') ? basename.slice(0, -1) : basename}_id`;
+				break;
+
+			case 'calendar':
+				this.keypath = 'service_id';
+				break;
+
+			case 'fare_attributes':
+				this.keypath = 'fare_id';
+				break;
+
+			case 'frequencies':
+				this.keypath = 'trip_id';
+				break;
+
+			case 'calendar_dates': case 'transfers': case 'feed_info':
+			case 'stop_times': case 'shapes': case 'fare_rules':
+			default:
+				this.keypath = null;
+				break;
+		}
+
+		this.first = true;
+  }
+
+	_transform(chunk, encoding, done) {
+		if (this.first) {
+			this.first = false;
+			this.push(this.keypath === null ? '[' : '{');
+		} else {
+			this.push(',');
+		}
+
+		if (encoding === 'buffer') chunk = JSON.parse(this.decoder.write(chunk));
+		else if (typeof chunk === 'string') chunk = JSON.parse(chunk);
+
+		let output = JSON.stringify(chunk)
+		if (this.keypath !== null) {
+			output = `${JSON.stringify(chunk[this.keypath])}:${output}`;
+		}
+		this.push(output);
+
+		done();
+	}
+
+	_flush(done) {
+		this.push(this.keypath === null ? ']' : '}');
+		done();
+	}
 }
 
 /**
@@ -40,41 +80,50 @@ function exists(path) {
  * @returns {Promise<void>} resolves when completed
  */
 module.exports = function extractGTFS(zipPath, outputDir) {
+	if (!zipPath || !outputDir) {
+		throw new TypeError('Missing parameter');
+	}
+
 	return new Promise((resolve, reject) => {
-		if (!zipPath || !outputDir) {
-			reject(new TypeError('Missing parameter'));
-		}
-
 		const outputStreams = [];
-		let foundErr = false;
 
-		function rejectAndEnd(err) {
-			reject(err);
-			foundErr = true;
-		}
+		mkdirp(outputDir, (err) => {
+			if (err) throw reject(err);
 
-		createReadStream(zipPath)
-			.pipe(Parse())
-			.on('error', rejectAndEnd)
+			createReadStream(zipPath).pipe(new Parse())
+			.on('error', (err) => {
+				parser.end();
+				reject(err);
+			})
 			.on('entry', (entry) => {
 				if (entry.type !== 'File' || extname(entry.path) !== '.txt') {
-					rejectAndEnd(new TypeError('Invalid GTFS zip file, ' +
-						'must not contain directories or non-txt files')
-					);
 					entry.autodrain();
+					parser.end();
+					reject(new TypeError('Invalid GTFS zip file, ' +
+						'must not contain directories or non-txt files'));
 				}
 
-				if (foundErr) {
-					entry.autodrain();
-					return;
-				}
+				const sourceFile = basename(entry.path, '.txt');
+				const destPath = join(outputDir, `${sourceFile}.json`);
 
-				const destPath = join(outputDir, `${entry.path.slice(0, -4)}.json`);
-				const output = promisePipe(entry, destPath);
-				outputStreams.push(output);
+				const writer = entry
+					.pipe(new Converter({
+						constructResult: false,
+						ignoreEmpty: true,
+						flatKeys: true,
+					}))
+					.pipe(new ArrayToObject(sourceFile))
+					.pipe(createWriteStream(destPath));
+
+				outputStreams.push(streamToPromise(writer).catch((err) => {
+					parser.end();
+					reject(err);
+				}));
+			})
+			.on('finish', () => {
+				resolve(Promise.all(outputStreams));
 			});
-
-		Promise.all(outputStreams).then(resolve, rejectAndEnd);
+		});
 	});
 }
 
