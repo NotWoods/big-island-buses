cbf75c7e78b6f611fa09637e81396464caaf590c
Tiger Oakes <contact@tigeroakes.com>
2019-03-17 00:17:42 +0000

    Debug SQL

--- src/server-render/api.ts
+++ src/server-render/api.ts
@@ -1,15 +1,16 @@
 import { promises as fs } from 'fs';
-import { join } from 'path';
+import { resolve, relative } from 'path';
 import { promise as alasql } from 'alasql';
 import { Omit, Route, Trip, StopTime, Stop } from './api-types';
 const { writeFile } = fs;
 
 // TODO
-const GTFS_FOLDER = './data';
-const API_FOLDER = './api';
+const GTFS_FOLDER = resolve(__dirname, '..', '..', 'static', 'google_transit');
+const API_FOLDER = resolve(__dirname, '..', '..', 'public', 'api');
 
 function csv(file: string) {
-    return `CSV(${join(GTFS_FOLDER, `${file}.csv`)}, {separator:","})`;
+    const path = relative(process.cwd(), resolve(GTFS_FOLDER, `${file}.txt`));
+    return `CSV('${path}', {separator:",", headers: true})`;
 }
 
 function toObject<T>(iter: Iterable<T>, key: keyof T) {
@@ -22,7 +23,7 @@ async function makeTripApi(route: Omit<Route, 'trip_ids'>) {
     const { route_id } = route;
     const tripsRes: Omit<Trip, 'stop_times'>[] = await alasql(
         `SELECT route_id, service_id, trip_id, trip_headsign AS headsign, trip_short_name AS name, direction_id
-        FROM CSV(${csv('trips')})
+        FROM ${csv('trips')}
         WHERE route_id='${route_id}'`,
     );
 
@@ -30,9 +31,9 @@ async function makeTripApi(route: Omit<Route, 'trip_ids'>) {
         tripsRes.map(async trip => {
             const stop_times: StopTime[] = await alasql(
                 `SELECT arrival_time AS time, stop_id
-                FROM CSV(${csv('stop_times')})
-                ORDER BY stop_sequence ASC
-                WHERE trip_id='${trip.trip_id}'`,
+                FROM ${csv('stop_times')}
+                WHERE trip_id='${trip.trip_id}'
+                ORDER BY stop_sequence ASC`,
             );
             (trip as Trip).stop_times = stop_times;
             return trip as Trip;
@@ -40,9 +41,13 @@ async function makeTripApi(route: Omit<Route, 'trip_ids'>) {
     );
 
     const tripMap = toObject(trips, 'trip_id');
-    const path = join(API_FOLDER, 'routes', `${route_id}.json`);
+    const path = resolve(API_FOLDER, 'routes', `${route_id}.json`);
 
-    await writeFile(path, JSON.stringify({ ...route, trips: tripMap }), 'utf8');
+    await writeFile(
+        path,
+        JSON.stringify({ ...route, trips: tripMap }, undefined, 2),
+        'utf8',
+    );
 
     return trips.map(trip => trip.trip_id);
 }
@@ -50,7 +55,7 @@ async function makeTripApi(route: Omit<Route, 'trip_ids'>) {
 async function makeRoutesApi() {
     const routesRes: Omit<Route, 'trip_ids'>[] = await alasql(
         `SELECT route_id, route_long_name AS name, route_url AS source_url, route_color AS color, route_text_color AS text_color
-        FROM CSV(${csv('routes')})
+        FROM ${csv('routes')}
         ORDER BY route_sort_order ASC`,
     );
 
@@ -63,24 +68,33 @@ async function makeRoutesApi() {
     );
 
     const routeMap = toObject(routes, 'route_id');
-    const path = join(API_FOLDER, 'routes.json');
-    await writeFile(path, JSON.stringify(routeMap), 'utf8');
+    const path = resolve(API_FOLDER, 'routes.json');
+    await writeFile(path, JSON.stringify(routeMap, undefined, 2), 'utf8');
 }
 
 async function makeStopsApi() {
-    const stopsRes: Omit<Stop, 'trips' | 'route_ids'>[] = await alasql(
-        `SELECT stop_id, stop_name AS name, stop_lat AS lat, stop_lon AS lon
-        FROM CSV(${csv('stops')})`,
-    );
+    const res = await Promise.all([
+        alasql(
+            `SELECT stop_id, stop_name AS name, stop_lat AS lat, stop_lon AS lon
+            FROM ${csv('stops')}`,
+        ) as Promise<Omit<Stop, 'trips' | 'route_ids'>[]>,
+        alasql(
+            `SELECT trip_id, route_id, direction_id
+            FROM ${csv('trips')}`,
+        ) as Promise<Pick<Trip, 'trip_id' | 'route_id' | 'direction_id'>[]>,
+    ]);
+    const stopsRes = res[0];
+    const tripMap = toObject(res[1], 'trip_id');
 
     const stops = await Promise.all(
         stopsRes.map(async stop => {
-            const trips: Stop['trips'] = await alasql(
-                `SELECT trips.trip_id, trips.route_id, trips.direction_id, stop_times.stop_sequence AS sequence, stop_times.arrival_time AS time
-                FROM CSV(${csv('stop_times')}) stop_times
-                JOIN CSV(${csv('trips')}) trips
-                ON stop_times.trip_id=trips.trip_id
-                WHERE stop_times.stop_id='${stop.stop_id}'`,
+            const stopTimes: Stop['trips'] = await alasql(
+                `SELECT trip_id, stop_sequence AS sequence, arrival_time AS time
+                FROM ${csv('stop_times')}
+                WHERE stop_id='${stop.stop_id}'`,
+            );
+            const trips = stopTimes.map(st =>
+                Object.assign(st, tripMap[st.trip_id]),
             );
             (stop as Stop).trips = trips;
             (stop as Stop).route_ids = Array.from(
@@ -91,8 +105,16 @@ async function makeStopsApi() {
     );
 
     const stopMap = toObject(stops, 'stop_id');
-    const path = join(API_FOLDER, 'stops.json');
-    await writeFile(path, JSON.stringify(stopMap), 'utf8');
+    const path = resolve(API_FOLDER, 'stops.json');
+    await writeFile(path, JSON.stringify(stopMap, undefined, 2), 'utf8');
 }
 
 async function makeCalendarApi() {}
+
+export async function main() {
+    await Promise.all([makeRoutesApi(), makeStopsApi(), makeCalendarApi()]);
+}
+
+if (require.main === module) {
+    main().catch(console.error);
+}
