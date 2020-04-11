5564e2537bc4d304071544ed99382d4a049a49b6
Tiger Oakes <contact@tigeroakes.com>
2020-04-10 22:26:13 -0700

    Change tab width

--- src/load.ts
+++ src/load.ts
@@ -8,39 +8,39 @@ import { GTFSData, Stop, Trip } from './gtfs-types';
 import { toInt } from './page/num';
 
 export const enum Type {
-    ROUTE,
-    STOP,
-    TRIP,
+  ROUTE,
+  STOP,
+  TRIP,
 }
 export const enum View {
-    LIST,
+  LIST,
 
-    MAP_PRIMARY,
-    STREET_PRIMARY,
+  MAP_PRIMARY,
+  STREET_PRIMARY,
 }
 
 export interface ActiveState {
-    Route: {
-        ID: string | null;
-        TRIP: string | null;
-    };
-    STOP: string | null;
-    View: {
-        ROUTE: View;
-        STOP: View;
-    };
+  Route: {
+    ID: string | null;
+    TRIP: string | null;
+  };
+  STOP: string | null;
+  View: {
+    ROUTE: View;
+    STOP: View;
+  };
 }
 
 export let Active: ActiveState = {
-    Route: {
-        ID: null,
-        TRIP: null,
-    },
-    STOP: null,
-    View: {
-        ROUTE: View.LIST,
-        STOP: View.MAP_PRIMARY,
-    },
+  Route: {
+    ID: null,
+    TRIP: null,
+  },
+  STOP: null,
+  View: {
+    ROUTE: View.LIST,
+    STOP: View.MAP_PRIMARY,
+  },
 };
 
 export const updateEvent = new CustomEvent('pageupdate');
@@ -53,74 +53,74 @@ navigator.serviceWorker?.register('service-worker.js');
 export const openCallbacks: Record<Type, Function> = {} as any;
 
 export const normal = {
-        url: 'assets/pins.png',
-        size: { height: 26, width: 24 },
-        scaledSize: { height: 26, width: 120 },
-        origin: { x: 0, y: 0 },
-        anchor: { x: 12, y: 12 },
-    } as google.maps.Icon,
-    unimportant = {
-        url: 'assets/pins.png',
-        size: { height: 26, width: 24 },
-        scaledSize: { height: 26, width: 120 },
-        origin: { x: 96, y: 0 },
-        anchor: { x: 12, y: 12 },
-    } as google.maps.Icon,
-    userShape = {
-        url: 'assets/pins.png',
-        size: { height: 26, width: 24 },
-        scaledSize: { height: 26, width: 120 },
-        origin: { x: 48, y: 0 },
-        anchor: { x: 12, y: 12 },
-    } as google.maps.Icon,
-    placeShape = {
-        url: 'assets/pins.png',
-        size: { height: 26, width: 24 },
-        scaledSize: { height: 26, width: 120 },
-        origin: { x: 72, y: 0 },
-        anchor: { x: 12, y: 23 },
-    } as google.maps.Icon,
-    stopShape = {
-        url: 'assets/pins.png',
-        size: { height: 26, width: 24 },
-        scaledSize: { height: 26, width: 120 },
-        origin: { x: 24, y: 0 },
-        anchor: { x: 12, y: 20 },
-    } as google.maps.Icon;
+    url: 'assets/pins.png',
+    size: { height: 26, width: 24 },
+    scaledSize: { height: 26, width: 120 },
+    origin: { x: 0, y: 0 },
+    anchor: { x: 12, y: 12 },
+  } as google.maps.Icon,
+  unimportant = {
+    url: 'assets/pins.png',
+    size: { height: 26, width: 24 },
+    scaledSize: { height: 26, width: 120 },
+    origin: { x: 96, y: 0 },
+    anchor: { x: 12, y: 12 },
+  } as google.maps.Icon,
+  userShape = {
+    url: 'assets/pins.png',
+    size: { height: 26, width: 24 },
+    scaledSize: { height: 26, width: 120 },
+    origin: { x: 48, y: 0 },
+    anchor: { x: 12, y: 12 },
+  } as google.maps.Icon,
+  placeShape = {
+    url: 'assets/pins.png',
+    size: { height: 26, width: 24 },
+    scaledSize: { height: 26, width: 120 },
+    origin: { x: 72, y: 0 },
+    anchor: { x: 12, y: 23 },
+  } as google.maps.Icon,
+  stopShape = {
+    url: 'assets/pins.png',
+    size: { height: 26, width: 24 },
+    scaledSize: { height: 26, width: 120 },
+    origin: { x: 24, y: 0 },
+    anchor: { x: 12, y: 20 },
+  } as google.maps.Icon;
 
 export function setActiveState(newState: ActiveState) {
-    Active = newState;
+  Active = newState;
 }
 
 /**
  * Grabs the API data and parses it into a GTFSData object for the rest of the program.
  */
 export function getScheduleData(): Promise<GTFSData> {
-    return fetch('api.json')
-        .then(res => {
-            if (res.ok) return res.json();
-            throw new Error(res.statusText);
-        })
-        .then(json => json as GTFSData);
+  return fetch('api.json')
+    .then(res => {
+      if (res.ok) return res.json();
+      throw new Error(res.statusText);
+    })
+    .then(json => json as GTFSData);
 }
 
 export function createElement<Tag extends keyof HTMLElementTagNameMap>(
-    type: Tag,
-    props: Partial<HTMLElementTagNameMap[Tag]>,
+  type: Tag,
+  props: Partial<HTMLElementTagNameMap[Tag]>,
 ) {
-    return Object.assign(document.createElement(type), props);
+  return Object.assign(document.createElement(type), props);
 }
 
 function getCurrentPosition() {
-    return new Promise<Position>((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+  return new Promise<Position>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
 export interface LocationUpdate {
-    stop: Stop['stop_id'];
-    location: Pick<Coordinates, 'latitude' | 'longitude'>;
-    custom: boolean;
+  stop: Stop['stop_id'];
+  location: Pick<Coordinates, 'latitude' | 'longitude'>;
+  custom: boolean;
 }
 
 /**
@@ -130,56 +130,56 @@ export interface LocationUpdate {
  */
 let runOnce = false;
 export function locateUser(
-    busPromise: Promise<GTFSData>,
-    customLocation?: Pick<Coordinates, 'latitude' | 'longitude'>,
+  busPromise: Promise<GTFSData>,
+  customLocation?: Pick<Coordinates, 'latitude' | 'longitude'>,
 ): Promise<LocationUpdate> {
-    let locatePromise: Promise<{
-        coords: Pick<Coordinates, 'latitude' | 'longitude'>;
-        customLocationFlag?: boolean;
-    }>;
-    if (customLocation) {
-        locatePromise = Promise.resolve({
-            coords: customLocation,
-            customLocationFlag: true,
-        });
-    } else {
-        locatePromise = getCurrentPosition();
-    }
-
-    let closestDistance = Number.MAX_VALUE;
-    let closestStop: Stop['stop_id'];
-
-    return Promise.all([locatePromise, busPromise]).then(([e, schedule]) => {
-        const userPos = e.coords;
-        for (const stop_id of Object.keys(schedule.stops)) {
-            const stop = schedule.stops[stop_id];
-            const distance = Math.sqrt(
-                Math.pow(userPos.latitude - parseFloat(stop.stop_lat), 2) +
-                    Math.pow(userPos.longitude - parseFloat(stop.stop_lon), 2),
-            );
-            if (distance < closestDistance) {
-                closestStop = stop_id;
-                closestDistance = distance;
-            }
-        }
-        if (closestStop) {
-            const results = {
-                stop: closestStop,
-                location: userPos,
-                custom: e.customLocationFlag ? true : false,
-            };
-            if (runOnce) {
-                window.dispatchEvent(
-                    new CustomEvent('locationupdate', {
-                        detail: results,
-                    }),
-                );
-            }
-            return results;
-        } else {
-            throw Error(JSON.stringify(userPos));
-        }
+  let locatePromise: Promise<{
+    coords: Pick<Coordinates, 'latitude' | 'longitude'>;
+    customLocationFlag?: boolean;
+  }>;
+  if (customLocation) {
+    locatePromise = Promise.resolve({
+      coords: customLocation,
+      customLocationFlag: true,
     });
+  } else {
+    locatePromise = getCurrentPosition();
+  }
+
+  let closestDistance = Number.MAX_VALUE;
+  let closestStop: Stop['stop_id'];
+
+  return Promise.all([locatePromise, busPromise]).then(([e, schedule]) => {
+    const userPos = e.coords;
+    for (const stop_id of Object.keys(schedule.stops)) {
+      const stop = schedule.stops[stop_id];
+      const distance = Math.sqrt(
+        Math.pow(userPos.latitude - parseFloat(stop.stop_lat), 2) +
+          Math.pow(userPos.longitude - parseFloat(stop.stop_lon), 2),
+      );
+      if (distance < closestDistance) {
+        closestStop = stop_id;
+        closestDistance = distance;
+      }
+    }
+    if (closestStop) {
+      const results = {
+        stop: closestStop,
+        location: userPos,
+        custom: e.customLocationFlag ? true : false,
+      };
+      if (runOnce) {
+        window.dispatchEvent(
+          new CustomEvent('locationupdate', {
+            detail: results,
+          }),
+        );
+      }
+      return results;
+    } else {
+      throw Error(JSON.stringify(userPos));
+    }
+  });
 }
 
 /**
@@ -187,20 +187,20 @@ export function locateUser(
  * @return {Promise<DocumentReadyState>} resolves if document has loaded
  */
 export function documentLoad() {
-    if (
-        document.readyState === 'interactive' ||
-        document.readyState === 'complete'
-    ) {
-        return Promise.resolve(document.readyState);
-    }
+  if (
+    document.readyState === 'interactive' ||
+    document.readyState === 'complete'
+  ) {
+    return Promise.resolve(document.readyState);
+  }
 
-    return new Promise(resolve => {
-        document.addEventListener('readystatechange', () => {
-            if (document.readyState === 'interactive') {
-                resolve(document.readyState);
-            }
-        });
+  return new Promise(resolve => {
+    document.addEventListener('readystatechange', () => {
+      if (document.readyState === 'interactive') {
+        resolve(document.readyState);
+      }
     });
+  });
 }
 
 /**
@@ -210,45 +210,45 @@ export function documentLoad() {
  * @return {string} URL to use for href, based on active object.
  */
 function pageLink(type: Type, value: string) {
-    const params = new URLSearchParams();
-    switch (type) {
-        case Type.ROUTE:
-            params.set('route', value);
+  const params = new URLSearchParams();
+  switch (type) {
+    case Type.ROUTE:
+      params.set('route', value);
 
-            if (Active.Route.TRIP !== null) {
-                params.set('trip', Active.Route.TRIP);
-            }
+      if (Active.Route.TRIP !== null) {
+        params.set('trip', Active.Route.TRIP);
+      }
 
-            if (Active.STOP !== null) {
-                params.set('stop', Active.STOP);
-            }
-            break;
-        case Type.STOP:
-            if (Active.Route.ID !== null) {
-                params.set('route', Active.Route.ID);
-            }
-            params.set('stop', value);
-            if (Active.Route.TRIP !== null) {
-                params.set('trip', Active.Route.TRIP);
-            }
-            break;
-        case Type.TRIP:
-            params.set('route', Active.Route.ID!);
-            params.set('trip', value);
-            if (Active.STOP !== null) {
-                params.set('stop', Active.STOP);
-            }
-            break;
-        default:
-            console.warn('Invalid type provided for link: %i', type);
-            break;
-    }
-    return `#!${params}`;
+      if (Active.STOP !== null) {
+        params.set('stop', Active.STOP);
+      }
+      break;
+    case Type.STOP:
+      if (Active.Route.ID !== null) {
+        params.set('route', Active.Route.ID);
+      }
+      params.set('stop', value);
+      if (Active.Route.TRIP !== null) {
+        params.set('trip', Active.Route.TRIP);
+      }
+      break;
+    case Type.TRIP:
+      params.set('route', Active.Route.ID!);
+      params.set('trip', value);
+      if (Active.STOP !== null) {
+        params.set('stop', Active.STOP);
+      }
+      break;
+    default:
+      console.warn('Invalid type provided for link: %i', type);
+      break;
+  }
+  return `#!${params}`;
 }
 
 export interface Linkable {
-    Type: Type;
-    Value: string;
+  Type: Type;
+  Value: string;
 }
 
 type DynamicLinkNode = HTMLAnchorElement & Linkable;
@@ -261,42 +261,42 @@ type DynamicLinkNode = HTMLAnchorElement & Linkable;
  * @return {Node}           A element with custom properties
  */
 export function dynamicLinkNode(type: Type, value: string, update?: boolean) {
-    const node = document.createElement('a') as DynamicLinkNode;
-    node.Type = type;
-    node.Value = value;
-    node.href = pageLink(type, value);
-    node.addEventListener('click', clickEvent);
-    if (update) {
-        node.addEventListener('pageupdate', function() {
-            node.href = pageLink(type, value);
-        });
-    }
+  const node = document.createElement('a') as DynamicLinkNode;
+  node.Type = type;
+  node.Value = value;
+  node.href = pageLink(type, value);
+  node.addEventListener('click', clickEvent);
+  if (update) {
+    node.addEventListener('pageupdate', function() {
+      node.href = pageLink(type, value);
+    });
+  }
 
-    return node;
+  return node;
 }
 
 /**
  * Navigate to the page described by the `Linkable`.
  */
 export function openLinkable(link: Linkable) {
-    const state = Active;
-    const val = link.Value;
-    const newLink = pageLink(link.Type, val);
-    const callback = openCallbacks[link.Type];
-    switch (link.Type) {
-        case Type.ROUTE:
-            state.Route.ID = val;
-            break;
-        case Type.STOP:
-            state.STOP = val;
-            break;
-        case Type.TRIP:
-            state.Route.TRIP = val;
-            break;
-    }
-    callback(val);
-    history.pushState(state, null as any, newLink);
-    ga?.('send', 'pageview', { page: newLink, title: document.title });
+  const state = Active;
+  const val = link.Value;
+  const newLink = pageLink(link.Type, val);
+  const callback = openCallbacks[link.Type];
+  switch (link.Type) {
+    case Type.ROUTE:
+      state.Route.ID = val;
+      break;
+    case Type.STOP:
+      state.STOP = val;
+      break;
+    case Type.TRIP:
+      state.Route.TRIP = val;
+      break;
+  }
+  callback(val);
+  history.pushState(state, null as any, newLink);
+  ga?.('send', 'pageview', { page: newLink, title: document.title });
 }
 
 /**
@@ -304,38 +304,38 @@ export function openLinkable(link: Linkable) {
  * @param  {Event} e
  */
 export function clickEvent(this: Linkable, e: Event) {
-    e.preventDefault?.();
-    e.stopPropagation?.();
-    openLinkable(this);
-    return false;
+  e.preventDefault?.();
+  e.stopPropagation?.();
+  openLinkable(this);
+  return false;
 }
 
 /**
  * Returns an object with URL variables.
  */
 export function getQueryVariables(): Partial<Record<string, string>> {
-    let query = '';
-    let vars: string[];
-    if (window.location.hash.indexOf('#!') > -1) {
-        query = window.location.hash.substring(
-            window.location.hash.indexOf('#!') + 2,
-        );
-        vars = query.split('&');
-    } else if (window.location.search.indexOf('_escaped_fragment_') > -1) {
-        query = window.location.search.substring(
-            window.location.search.indexOf('_escaped_fragment_') + 19,
-        );
-        vars = query.split('%26');
-    }
+  let query = '';
+  let vars: string[];
+  if (window.location.hash.indexOf('#!') > -1) {
+    query = window.location.hash.substring(
+      window.location.hash.indexOf('#!') + 2,
+    );
+    vars = query.split('&');
+  } else if (window.location.search.indexOf('_escaped_fragment_') > -1) {
+    query = window.location.search.substring(
+      window.location.search.indexOf('_escaped_fragment_') + 19,
+    );
+    vars = query.split('%26');
+  }
 
-    const result: Partial<Record<string, string>> = {};
-    if (query !== '') {
-        for (const parts of vars!) {
-            const [key, value] = parts.split('=');
-            result[key] = value;
-        }
+  const result: Partial<Record<string, string>> = {};
+  if (query !== '') {
+    for (const parts of vars!) {
+      const [key, value] = parts.split('=');
+      result[key] = value;
     }
-    return result;
+  }
+  return result;
 }
 
 /**
@@ -344,9 +344,9 @@ export function getQueryVariables(): Partial<Record<string, string>> {
  * @return ordered list
  */
 export function sequence(stopTimes: Trip['stop_times']): string[] {
-    const stopSequence = [];
-    for (const key in stopTimes) {
-        stopSequence.push(key);
-    }
-    return stopSequence.sort((a, b) => toInt(a) - toInt(b));
+  const stopSequence = [];
+  for (const key in stopTimes) {
+    stopSequence.push(key);
+  }
+  return stopSequence.sort((a, b) => toInt(a) - toInt(b));
 }
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,51 +3,51 @@ const CACHE = 'network-or-cache';
 export declare var self: ServiceWorkerGlobalScope;
 
 self.addEventListener('install', evt => {
-    evt.waitUntil(precache());
+  evt.waitUntil(precache());
 });
 
 self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        fromNetwork(evt.request, 400).catch(() => fromCache(evt.request)),
-    );
+  evt.respondWith(
+    fromNetwork(evt.request, 400).catch(() => fromCache(evt.request)),
+  );
 });
 
 function precache() {
-    return caches
-        .open(CACHE)
-        .then(cache =>
-            cache.addAll([
-                './',
-                'index.html',
-                'style.css',
-                'main.js',
-                'api.json',
-                'assets/logo.svg',
-                'assets/lines.svg',
-                'assets/tigeroakes.svg',
-            ]),
-        );
+  return caches
+    .open(CACHE)
+    .then(cache =>
+      cache.addAll([
+        './',
+        'index.html',
+        'style.css',
+        'main.js',
+        'api.json',
+        'assets/logo.svg',
+        'assets/lines.svg',
+        'assets/tigeroakes.svg',
+      ]),
+    );
 }
 
 function fromNetwork(request: RequestInfo, timeout: number) {
-    return new Promise<Response>((resolve, reject) => {
-        const timeoutId = setTimeout(reject, timeout);
-        fetch(request).then(response => {
-            clearTimeout(timeoutId);
-            resolve(response);
-        }, reject);
-    });
+  return new Promise<Response>((resolve, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      resolve(response);
+    }, reject);
+  });
 }
 
 function fromCache(request: RequestInfo) {
-    return caches
-        .open(CACHE)
-        .then(cache => cache.match(request))
-        .then(matching => {
-            if (matching) {
-                return matching;
-            } else {
-                throw new Error('no-match');
-            }
-        });
+  return caches
+    .open(CACHE)
+    .then(cache => cache.match(request))
+    .then(matching => {
+      if (matching) {
+        return matching;
+      } else {
+        throw new Error('no-match');
+      }
+    });
 }
