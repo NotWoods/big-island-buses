f69b315f32cba7eb01e6948dc412f91325f99534
Tiger Oakes <contact@tigeroakes.com>
2020-04-11 11:58:42 -0700

    Update service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -10,15 +10,25 @@ self.addEventListener('fetch', evt => {
   const url = new URL(evt.request.url);
   if (url.host === location.host) {
     if (url.pathname.includes('/routes/')) {
+      // Route pages have same layout as main page
       evt.respondWith(
-        fromCache('index.html')
-      )
+        fromNetwork(evt.request, 400).catch(() => fromCache('index.html')),
+      );
     } else {
+      // Network then fallback
       evt.respondWith(
-        fromNetwork(evt.request, 400).catch(() => fromCache(evt.request)),
+        fromNetwork(evt.request, 400).then(
+          response => {
+            // Update cache after network success
+            evt.waitUntil(putInCache(evt.request, response));
+            return response;
+          },
+          () => fromCache(evt.request),
+        ),
       );
     }
   } else {
+    // External scripts (Google Maps) aren't cached
     evt.respondWith(fetch(evt.request));
   }
 });
@@ -36,16 +46,19 @@ function precache() {
         'assets/logo.svg',
         'assets/lines.svg',
         'assets/tigeroakes.svg',
+        'icon/favicon.ico',
+        'icon/maskable.png',
+        'icon/transparent.png',
       ]),
     );
 }
 
 function fromNetwork(request: RequestInfo, timeout: number) {
-  return new Promise<Response>((resolve, reject) => {
+  return new Promise<Response>((fulfill, reject) => {
     const timeoutId = setTimeout(reject, timeout);
     fetch(request).then(response => {
       clearTimeout(timeoutId);
-      resolve(response);
+      fulfill(response);
     }, reject);
   });
 }
@@ -58,7 +71,13 @@ function fromCache(request: RequestInfo) {
       if (matching) {
         return matching;
       } else {
-        throw new Error('no-match');
+        throw new Error(`${request} not cached`);
       }
     });
+}
+
+function putInCache(request: RequestInfo, response: Response) {
+  return caches.open(CACHE).then(cache => {
+    return cache.put(request, response);
+  });
 }
